5af35d985c32650da9c0c2dc2beb734c553fc428
Grandia Muhammad <grandsteve.gm@gmail.com>
2025-04-05 23:27:33 +0700

    membuat textfield untuk inputan pengguna

--- app/src/main/java/com/grandiamuhammad3096/assessment01/ui/screen/MainScreen.kt
+++ app/src/main/java/com/grandiamuhammad3096/assessment01/ui/screen/MainScreen.kt
@@ -1,17 +1,35 @@
 package com.grandiamuhammad3096.assessment01.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.grandiamuhammad3096.assessment01.R
 import com.grandiamuhammad3096.assessment01.ui.theme.Assessment01Theme
 
@@ -31,16 +49,42 @@ fun MainScreen() {
             )
         }
     ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
+        KonversiSuhu(Modifier.padding(innerPadding))
     }
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+fun KonversiSuhu(modifier: Modifier = Modifier) {
+    var inputNilaiSuhu by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.judul_aplikasi),
+            style = MaterialTheme.typography.bodyLarge,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = inputNilaiSuhu,
+            onValueChange = { inputNilaiSuhu = it },
+            label = { Text(text = stringResource(R.string.keterangan_label_suhu)) },
+            trailingIcon = { Text(text = "\u00B0") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
